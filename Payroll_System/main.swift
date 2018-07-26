09d19eea25bc5b0d525c3b2b7212e169ba290d9e
MacStudent <macstudent@20x-imacxx-m-481.local>
2018-07-26 16:11:48 -0400

    Initial Commit

---
+++ Payroll_System/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Payroll_System
+//
+//  Created by MacStudent on 2018-07-26.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
